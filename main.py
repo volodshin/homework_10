002d15386a22da50689e8ff4682144241af82d69
V0lodimir <v.shinkarenko05@gmail.com>
2022-09-04 19:16:42 +0300

    new program

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import os
-import json
+my_list = ["Vasya", "Andry", "Petro"]
 
-DIR = "files"
-python_data = [{"name": "John", "age": 18},{"name": "John", "age": 25}]
+def print_mylist():
+   for x in my_list:
+      x = my_list[0]
+   print(x)
 
-with open(os.path.join(DIR, 'users.json'), 'w', encoding="UTF-8") as f:
-   json.dump(python_data, f, ensure_ascii=False)
-
-with open(os.path.join(DIR, 'users.json'), 'r', encoding="UTF-8") as f:
-   read_data = json.load(f)
-
-print(read_data[0]['name'])
-
+print_mylist()
